9a7bd650c53a2c224475187d90fb1303f902804f
Maarten Inja <maarten.inja@gmail.com>
2014-11-03 15:04:10 +0100

    more test in tmp.py

--- ex1/tmp.py
+++ ex1/tmp.py
@@ -7,5 +7,8 @@ plt.plot([0], [2], 'gp')
 plt.plot([1], [0], 'gh')
 plt.plot([1], [1], 'g8') # ?
 plt.plot([1], [2], 'o', color='#0000FF') # hexastring circle
+plt.plot([2], [0], 'o', color='#0000FF', markersize=2) # hexastring circle
+plt.plot([2], [1], 'o', color='#0000FF', markersize=6) # hexastring circle
+plt.plot([2], [2], 'o', color='#0000FF', markersize=16) # hexastring circle
 plt.axis([-.5, 3.5, -.5, 3.5])
 plt.show()
