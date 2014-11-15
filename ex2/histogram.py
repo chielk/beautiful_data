6ed80fccb5f8c217d475d0779f65ca4d8f295007
Chiel Kooijman <chiel999@gmail.com>
2014-11-15 14:46:29 +0100

    Better histogram

--- ex2/histogram.py
+++ ex2/histogram.py
@@ -5,9 +5,9 @@ a = np.fromfile("data/slice.1", dtype=np.dtype(np.ushort))
 for i in range(2, 95):
     a = np.hstack((a, np.fromfile("data/slice.%d" % i, dtype=np.dtype(np.ushort))))
 
-hist, bins = np.histogram(a, bins=100000)
+hist, bins = np.histogram(a, bins=65536)
 hist[0] = 0
 
-plt.bar(range(3500), hist[:3500], align='center')
+plt.bar(range(2500), hist[:2500], align='center')
 plt.show()
 #print hist
