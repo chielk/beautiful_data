230923354a6fd3c142f9d088b14b9f32b5ff2d6a
Maarten Inja <maarten.inja@gmail.com>
2014-11-15 18:15:10 +0100

    outline (11) done

--- ex2/prog.py
+++ ex2/prog.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     max_value = data.GetScalarTypeMax()
 
     settings = [ ( (200, 250), (0.7, 0, 0), 0.1),        # skin/hair
-                 #( (1800, 2500), (0, 0.7, 0), 0.1),      # skeleton
-                 #( (3090, 30000), (0, 0, 0.7), 0.1)      # jaw, teeth and coin?
+                 ( (1800, 2500), (0, 0.7, 0), 0.1),      # skeleton
+                 ( (3090, 30000), (0, 0, 0.7), 0.1)      # jaw, teeth and coin?
                 ]
 
     actors = []
@@ -52,20 +52,30 @@ if __name__ == '__main__':
 
         actors.append(data_actor)
 
-    # here we do all rendering (has basically nothing to do with the visualization)
+    # to plot the outline
+    outline = vtk.vtkOutlineFilter()
+    outline.SetInput(data)
+    outline_mapper = vtk.vtkPolyDataMapper()
+    outline_mapper.SetInputConnection(outline.GetOutputPort())
+    outline_actor = vtk.vtkOpenGLActor()
+    outline_actor.SetMapper(outline_mapper)
+    outline_actor.GetProperty().SetColor(1, 1, 1)
+
+
+    # below this point we do all rendering (has basically nothing to do with the visualization)
     renderer = vtk.vtkRenderer()
     renderer.SetBackground(0.33, 0.35, 0.43)
 
     # TODO: I try to set the camera here. but oh vtk :(
     #renderer.GetActiveCamera().SetPosition(0, 0, 0)
-    print renderer.GetActiveCamera().GetEyeAngle()
+    #print renderer.GetActiveCamera().GetEyeAngle()
     #renderer.GetActiveCamera().Elevation(-0.1)
-    renderer.UpdateCamera()
+    #renderer.UpdateCamera()
 
     # do not forget to add the actors here:
     for actor in actors:
         renderer.AddActor(actor)
-    #renderer.AddActor(volume)
+    renderer.AddActor(outline_actor)
 
     window = vtk.vtkRenderWindow()
     window.SetSize(500, 500)
