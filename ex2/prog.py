0f494fd6ccfcbac7784787b2b1988249d3605c6b
Maarten Inja <maarten.inja@gmail.com>
2014-11-15 16:48:25 +0100

    rgb for skin, skeleton, teeht and key

--- ex2/prog.py
+++ ex2/prog.py
@@ -21,36 +21,20 @@ if __name__ == '__main__':
     min_value = data.GetScalarTypeMin()
     max_value = data.GetScalarTypeMax()
 
-    settings = [ ( (190.0, 200.0), (0.7, 0, 0), 0.1),
-                 ( (200, 250), (0, 0.7, 0.7), 0.1),
-                 ( (1900, 2100), (0, 0.7, 0), 0.1),
-                 ( (2900, 3100), (0, 0, 0.7), 0.1)]
-                 #( (max_value * 0.9, max_value), (0, 0.7, 0), 0.4)]
-                 #( (max_value * 0.83, max_value * 0.9), (0, 0, 0.7), 0.4)]
-
-    settings2 = [ ( 700, (0.7, 0, 0), 0.2),
-                 ( 2000, (0, 0.7, 0), 0.4)]
-                 #( (max_value * 0.9, max_value), (0, 0.7, 0), 0.4)]
-                 #( (max_value * 0.83, max_value * 0.9), (0, 0, 0.7), 0.4)]
+    settings = [ ( (200, 250), (0.7, 0, 0), 0.1),       # skin/hair
+                 ( (1900, 2100), (0, 0.7, 0), 0.2),     # skeleton
+                 ( (3090, 3100), (0, 0, 0.7), 0.4)      # jaw, teeth and coin?
+                ]
 
     actors = []
 
-    i = 0
     for min_max, color, opacity in settings:
-
-
-        print 'min max', min_max
-        print 'color', color
-        print 'opacity', opacity
 
         # contour
         data_contour = vtk.vtkMarchingCubes()
-        #data_contour.SetInputConnection(data.GetOutputPort())
         data_contour.SetInput(data)
         data_contour.ComputeNormalsOn()
         data_contour.GenerateValues(5, min_max[0], min_max[1])
-        #data_contour.SetValue(i, min_max)
-        i += 1
         data_contour.Update()
 
         # geometry
@@ -65,36 +49,6 @@ if __name__ == '__main__':
         data_actor.GetProperty().SetOpacity(opacity)
 
         actors.append(data_actor)
-
-
-    ## color (for the volume property)
-    #volume_color = vtk.vtkColorTransferFunction()
-    #volume_color.AddRGBPoint(1, 0.7, 0, 0)
-    #volume_color.AddRGBPoint(2, 0, 0.7, 0)
-
-    ## for the opacity (for the volume property)
-    #volume_opacity = vtk.vtkPiecewiseFunction()
-    #volume_opacity.AddPoint(1, 0.1)
-    #volume_opacity.AddPoint(2, 0.2)
-
-    ## create the volume property (for the volume)
-    #volume_property = vtk.vtkVolumeProperty()
-    #volume_property.SetColor(volume_color)
-    #volume_property.SetScalarOpacity(volume_opacity)
-
-    ## render function (for the mapper)
-    #render_function = vtk.vtkVolumeRayCastCompositeFunction()
-
-    ## mapper (volume)
-    #volume_mapper = vtk.vtkVolumeRayCastMapper()
-    #volume_mapper.SetVolumeRayCastFunction(render_function)
-    #volume_mapper.SetInput(data)
-
-    ## actual volume (this is an actor)
-    #volume = vtk.vtkVolume()
-    #volume.SetMapper(volume_mapper)
-    #volume.SetProperty(volume_property)
-
 
     # here we do all rendering (has basically nothing to do with the visualization)
     renderer = vtk.vtkRenderer()
